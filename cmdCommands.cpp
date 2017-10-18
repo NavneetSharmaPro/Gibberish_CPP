39089d2825466ac9580291ad6ffe74ae80464622
Navneet <navneet.sharma.pro@gmail.com>
2017-10-18 10:51:37 +0530

    how to send cmd commands via cpp code

---
+++ cmdCommands.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdlib> //to use command prompt
+//#include <dos.h>
+//#include <windows.h>
+//#include <conio.h>
+//#include<chrono>
+using namespace std;
+int main()
+{
+  system("dir");
+  /*system("dir");//It was INSANELY EASY to do that ! and I was a fool thinking it'd be tough;
+  system("osk");
+  system("powershell");
+  sleep(2000);
+  system("ls");
+  cin.get();*/
+  cin.get();
+  return 0;
+}
